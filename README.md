6d15bd405693940974dac818201a8510f32d844b
Krisztian Kugler <kugler.krisztian@erise.hu>
2019-03-25 09:36:32 +0100

    refactor

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# Funeral Zone Javascript Code Test 
+# Funeral Zone Javascript Code Test
 
-You need to refactor this class, it is a simple class that makes a call to a http API to retrieve a list of books and return them. Think about extensibility - how could you easily add other book seller APIs in the the future, handle different API formats, different query types (by publisher, by year published etc - things like that). 
+You need to refactor this class, it is a simple class that makes a call to a http API to retrieve a list of books and return them. Think about extensibility - how could you easily add other book seller APIs in the the future, handle different API formats, different query types (by publisher, by year published etc - things like that).
 
 Refactor this to what you consider to be production ready code.
 
 ## Getting started
+
 Simply fork this repository and commit and push your changes to yor fork.
+
+---
+
+# Refactor notes
+
+- Switched from vanilla JavaScript to TypeScript.
+- Replaced the old function constructor syntax with a TypeScript class, which is basically an ES6 class with slightly more features.
+- Switched from XMLHttpRequest to Fetch API. It's not necessarily better but provides a cleaner syntax.
+- My main goal was to keep things short, clean and concise as well as to use modern solutions.
+
+# Refactored class features
+
+- You can instantiate the class by providing an API URL and a config object that contains the necessary query parameters in a 'key:value' format.
+- The 'getBooks' method makes an API call with the given URL and the stored config object. You may also give it a new config object in which case it'll be used instead of the stored one.
+- There's no need to declare a response format anymore, it will be figured out automatically from the response headers (support for JSON and Text/XML).
+- Since the stored API URL and config object are public properties, they can be changed at will.
---
+++ src/GetBookList.ts
@@ -0,0 +1,66 @@
+class GetBookList {
+  constructor(public url: string, public config: Object) {}
+
+  public getBooks(params?: Object): void {
+    let config: Object;
+    params ? (config = params) : (config = this.config);
+    const url = this.generateRequestURL(this.url, config);
+    fetch(url)
+      .then(this.responseHandler)
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+  // This method takes any parameters and merges them properly into the base URL to build the final URL string for the request.
+  private generateRequestURL(url: string, params: Object): string {
+    url.endsWith("/") ? (url += "?") : (url += "/?");
+    let i: number = 1; // To keep track of the iteration in the loop below.
+    for (let key in Object.keys(params)) {
+      i === 1 ? (url += `${key}=${params[key]}`) : (url += `&${key}=${params[key]}`);
+      i++;
+    }
+    return url;
+  }
+
+  private responseHandler(response: Response): Promise<any> {
+    const contentType = response.headers.get("content-type");
+    if (contentType.includes("application/json")) {
+      return this.JSONResponseHandler(response);
+    } else if (contentType.includes("text/html")) {
+      return this.textResponseHandler(response);
+    } else {
+      throw new Error(`Content-type '${contentType}' is not supported.`);
+    }
+  }
+
+  private JSONResponseHandler(response: Response): Promise<any> {
+    return response.json().then(json => {
+      if (response.ok) {
+        return json;
+      } else {
+        return Promise.reject(
+          Object.assign({}, json, {
+            status: response.status,
+            statusText: response.statusText
+          })
+        );
+      }
+    });
+  }
+
+  private textResponseHandler(response: Response): Promise<any> {
+    return response.text().then(text => {
+      if (response.ok) {
+        return new DOMParser().parseFromString(text, "application/xml");
+      } else {
+        throw new Error(
+          `
+          Response error!
+          Status: ${response.status}
+          StatusText: ${response.statusText}
+          `
+        );
+      }
+    });
+  }
+}
